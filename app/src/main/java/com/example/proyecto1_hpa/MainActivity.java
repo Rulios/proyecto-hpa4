a3120c6b87528e6936f4ddbd5df1e214ef5aee50
Rulios <robert_lu20@hotmail.com>
2023-11-11 23:59:18 -0500

    Viewpager functioning

--- app/src/main/java/com/example/proyecto1_hpa/MainActivity.java
+++ app/src/main/java/com/example/proyecto1_hpa/MainActivity.java
@@ -8,10 +8,19 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import androidx.appcompat.widget.Toolbar;
 
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar TB;
+
+
+
+    private ViewPager2 viewPagerValores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
 
         TB=findViewById(R.id.TB);
         setSupportActionBar(TB);
+
+        //código para llenar la info del viewpager
+        viewPagerValores = findViewById(R.id.viewPager2);
+        List <Datos> lista;
+        lista = new ArrayList<Datos>();
+
+        lista.add(new Datos(1, "VALOR #1", "CONTENIDO TEXTO VALOR...", R.drawable.valor_test));
+        lista.add(new Datos(2, "VALOR #2", "CONTENIDO TEXTO VALOR...", R.drawable.valor_test));
+        lista.add(new Datos(3, "VALOR #3", "CONTENIDO TEXTO VALOR...", R.drawable.valor_test));
+        lista.add(new Datos(4, "VALOR #4", "CONTENIDO TEXTO VALOR...", R.drawable.valor_test));
+        lista.add(new Datos(5, "VALOR $5", "CONTENIDO TEXTO VALOR...", R.drawable.valor_test));
+
+        Adaptador viewPagerAdaptador = new Adaptador( getSupportFragmentManager(), getLifecycle(), lista);
+        viewPagerValores.setAdapter(viewPagerAdaptador);
 
     }
 
---
+++ app/src/main/java/com/example/proyecto1_hpa/ViewPagerFragment.java
@@ -0,0 +1,35 @@
+package com.example.proyecto1_hpa;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ViewPagerFragment extends Fragment {
+    final Datos data;
+
+    public ViewPagerFragment(Datos data) {
+        this.data = data;
+    }
+
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.informacion, container, false);
+
+        // Bind data to your view
+        TextView titleTextView = view.findViewById(R.id.textView);
+        TextView descriptionTextView = view.findViewById(R.id.textView2);
+        ImageView imagen = view.findViewById(R.id.imageView);
+
+        titleTextView.setText(data.getTitulo());
+        descriptionTextView.setText(data.getDetalle());
+        imagen.setImageResource(data.getImagen());
+
+        return view;
+    }
+}
