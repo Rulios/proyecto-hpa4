43ee6622541aee074bcb9de8eee76ea7925867c7
Rulios <robert_lu20@hotmail.com>
2023-11-17 10:36:06 -0500

    Added comentarios

--- app/src/main/java/com/example/proyecto1_hpa/ViewPagerFragment.java
+++ app/src/main/java/com/example/proyecto1_hpa/ViewPagerFragment.java
@@ -9,23 +9,29 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+//Clase para crear el fragmento del slide e inflarlo con los datos correspondientes en el layout informacion.xml
 public class ViewPagerFragment extends Fragment {
+
+    //propiedades de la clase
     final Valores data;
 
+    //constructor de la clase 
     public ViewPagerFragment(Valores data) {
-        this.data = data;
+        this.data = data; //inicialización de los datos en la propiedad de la clase
     }
 
 
+    //infla los datos en el layout de informacion.xml en su espacio correspondiente
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.informacion, container, false);
 
-        // Bind data to your view
+        // registro de los espacios (componentes) correspondientes
         TextView titleTextView = view.findViewById(R.id.textView);
         TextView descriptionTextView = view.findViewById(R.id.textView2);
         ImageView imagen = view.findViewById(R.id.imageView);
 
+        //asignación de los valores en los componentes
         titleTextView.setText(data.getNombre());
         descriptionTextView.setText(data.getDescripcion());
         imagen.setImageResource(data.getImagen());
